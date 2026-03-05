d3d6b30ad58f3676431cb7106e5f727f5fd5a3ba
Dhruv Tiwari <tiwaridhruv15@gmail.com>
2026-03-05 14:00:02 +0530

    Extensive Model Training Utils and training begins

--- networksecurity/components/model_training.py
+++ networksecurity/components/model_training.py
@@ -4,18 +4,50 @@ import numpy as np
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import (RandomForestClassifier, GradientBoostingClassifier)
 from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
 
 from networksecurity.entity.config_entity import ModelTrainerConfig
 from networksecurity.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact
 from networksecurity.utils.main_utils.utils import save_pickle_object, load_numpy_array, load_pickle_object
-from networksecurity.utils.ml_utils.classification_metrics import ClassificationMetrics
+from networksecurity.utils.ml_utils.classification_metrics import get_classification_score, evaluate_model
 from networksecurity.utils.ml_utils.network_model import NetworkModel
 from networksecurity.exceptions.exceptions import NetworkSecurityException
 from networksecurity.logger.logger import logging
 
+
+class ModelTrainer:
+    def __init__(self, transformation_artifact: DataTransformationArtifact, trainer_config: ModelTrainerConfig):
+        self.model_input = transformation_artifact
+        self.model_config = trainer_config
+    
+    def train_evaluate_model(self, X_train, X_test, y_train, y_test):
+      model_report:dict = evaluate_model(X_train, X_test, y_train, y_test)
+      
+      best_model_accuracy = max(sorted(list(model_report.values())))
+      best_model = list(model_report.keys())[list(model_report.values()).index(best_model_accuracy)]
+
+      y_train_pred = best_model.predict(X_train)
+      clasification_train_metric = get_classification_score(y_train, y_train_pred)
+      
+      y_test_pred = best_model.predict(X_test)
+      classification_test_metric = get_classification_score(y_test, y_test_pred)
+
+    
+    def initiate_model_training(self)->ModelTrainerArtifact:
+        train_arr = load_numpy_array(self.model_input.transformed_train_path)
+        test_arr = load_numpy_array(self.model_input.transformed_test_path)
+        preprocessor = load_pickle_object(self.model_input.preprocessor_path)
+
+        X_train, X_test, y_train, y_test = (
+            train_arr[:, :-1],
+            test_arr[:, :-1],
+            train_arr[:, -1],
+            test_arr[:, -1]
+        )
 
 
 
--- networksecurity/utils/ml_utils/classification_metrics.py
+++ networksecurity/utils/ml_utils/classification_metrics.py
@@ -1,24 +1,75 @@
 import os, sys
 from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import (RandomForestClassifier, GradientBoostingClassifier)
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
 
 from networksecurity.entity.artifact_entity import ClassificationMetricArtifact
 from networksecurity.exceptions.exceptions import NetworkSecurityException
 
-class ClassificationMetrics:
-    def __init__(self, y_true, y_pred):
-        self.y_true = y_true
-        self.y_pred = y_pred
-    
-    def get_classification_score(self)->ClassificationMetricArtifact:
-        try:
-            model_f1 = f1_score(self.y_true, self.y_pred)
-            model_precision = precision_score(self.y_true, self.y_pred)
-            model_recall = recall_score(self.y_true, self.y_pred)
 
-            return ClassificationMetricArtifact(
-                f1_score=model_f1,
-                precision=model_precision,
-                recall=model_recall
-            )
-        except Exception as e:
+def get_classification_score(y_true, y_pred)->ClassificationMetricArtifact:
+    try:
+        model_f1 = f1_score(y_true, y_pred)
+        model_precision = precision_score(y_true, y_pred)
+        model_recall = recall_score(y_true, y_pred)
+
+        return ClassificationMetricArtifact(
+            f1_score=model_f1,
+            precision=model_precision,
+            recall=model_recall
+        )
+    except Exception as e:
             raise NetworkSecurityException(e, sys)
+
+def evaluate_model(X_train, X_test, y_train, y_test):
+        try:
+            models = {
+                "Logistic Regression": LogisticRegression(),
+                "KNN": KNeighborsClassifier(),
+                    "Decision Tree": DecisionTreeClassifier(),
+                    "Random Forest": RandomForestClassifier(),
+                    "Gradient Boosting": GradientBoostingClassifier(),
+                    "XGBoost": XGBClassifier()
+                }
+
+            params={
+                "KNN":{
+                    "n_neighbors":[4,5,6]
+                },
+                "Decision Tree":{
+                    "criterion":["gini", "entropy", "log_loss"]
+                },
+                "Random Forest":{
+                    "n_estimators":[8,16,32,64,128]
+                },
+                "Gradient Boosting":{
+                    "learning_rate": [0.01, 0.1, 0.5]
+                },
+                "XGBoost": {
+                    "eta":[ 0.2, 0.5],
+                    "max_depth": [5,6]
+                }
+            }
+            report = {}
+            for i in range(len(list(models))):
+                model = list(models.values())[i]
+                param = list(params.values())[i]
+                gs = GridSearchCV(model, param_grid=param, cv=3)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_score_)
+                model.fit(X_train, y_train)
+
+                y_test_pred = model.predict(X_test)
+                test_accuracy = accuracy_score(y_true=y_test, y_pred=y_test_pred)
+                report[list(models.keys())[i]] = test_accuracy
+                return report
+        
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)    
+            
