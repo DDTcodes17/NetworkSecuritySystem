b3bf5f92199e38d757dc14c88006226027eedaff
Dhruv Tiwari <tiwaridhruv15@gmail.com>
2026-03-10 19:54:12 +0530

    Added Custom Exception in main code

--- networksecurity/pipelines/training_pipeline.py
+++ networksecurity/pipelines/training_pipeline.py
@@ -1,3 +1,5 @@
+import sys
+
 from networksecurity.constant import training_config
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
@@ -22,36 +24,48 @@ class TrainingPipeline:
         self.training_pipeline_config = TrainingConfig()
     
     def start_data_ingestion(self):
-        logging.info("Initiating Data Ingestion Configuration.")
-        self.ingestion_config = DataIngestionConfig(training_par_config=self.training_pipeline_config)
-        ingestion_obj = DataIngestion(self.ingestion_config)
-        ingestion_artifact = ingestion_obj.initiate_data_ingestion()
-        logging.info("Ingestion Completed.")
-        return ingestion_artifact
+        try:
+            logging.info("Initiating Data Ingestion Configuration.")
+            self.ingestion_config = DataIngestionConfig(training_par_config=self.training_pipeline_config)
+            ingestion_obj = DataIngestion(self.ingestion_config)
+            ingestion_artifact = ingestion_obj.initiate_data_ingestion()
+            logging.info("Ingestion Completed.")
+            return ingestion_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
     
     def start_data_validation(self, ingestion_artifact: DataIngestionArtifact):
-        logging.info("Data Validation Initiated.")
-        self.validation_config = DataValidationConfig(self.training_pipeline_config)
-        validation_obj = DataValidation(data_validation_config=self.validation_config,
-                                        data_ingestion_artifact=ingestion_artifact)
-        validation_artifact = validation_obj.initiate_data_validation()
-        logging.info("Data Validation Completed.")
-        return validation_artifact
-    
+        try:
+            logging.info("Data Validation Initiated.")
+            self.validation_config = DataValidationConfig(self.training_pipeline_config)
+            validation_obj = DataValidation(data_validation_config=self.validation_config,
+                                            data_ingestion_artifact=ingestion_artifact)
+            validation_artifact = validation_obj.initiate_data_validation()
+            logging.info("Data Validation Completed.")
+            return validation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
     def start_data_transformation(self, validation_artifact:DataValidationArtifact):
-        logging.info("Data Transformation Inititated.")
-        self.transformation_config = DataTransformationConfig(self.training_pipeline_config)
-        transformation_obj = DataTranformation(validation_artifact=validation_artifact,
-                                               transformation_config=self.transformation_config)
-        transformation_artifact = transformation_obj.initiate_transformation()
-        logging.info("Data transformation Completed.")
-        return transformation_artifact
+        try:
+            logging.info("Data Transformation Inititated.")
+            self.transformation_config = DataTransformationConfig(self.training_pipeline_config)
+            transformation_obj = DataTranformation(validation_artifact=validation_artifact,
+                                                transformation_config=self.transformation_config)
+            transformation_artifact = transformation_obj.initiate_transformation()
+            logging.info("Data transformation Completed.")
+            return transformation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
     
     def start_model_training(self, transformation_artifact:DataTransformationArtifact):
-        logging.info("Model Training Initiated.")
-        self.model_training_config = ModelTrainerConfig(self.training_pipeline_config)
-        model_training_obj = ModelTrainer(trainer_config=self.model_training_config,
-                                          transformation_artifact=transformation_artifact)
-        model_trainer_artifact = model_training_obj.initiate_model_training()
-        logging.info("Model Trained.")
-        return model_trainer_artifact
+        try:
+            logging.info("Model Training Initiated.")
+            self.model_training_config = ModelTrainerConfig(self.training_pipeline_config)
+            model_training_obj = ModelTrainer(trainer_config=self.model_training_config,
+                                            transformation_artifact=transformation_artifact)
+            model_trainer_artifact = model_training_obj.initiate_model_training()
+            logging.info("Model Trained.")
+            return model_trainer_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
